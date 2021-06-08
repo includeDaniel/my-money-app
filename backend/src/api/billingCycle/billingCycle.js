9d3accf50f2b83381ec1898ed8f6dbef46fdc597
Daniel Nunes <danielzcomputacao@gmail.com>
2021-06-08 16:51:00 -0300

    billingCycle

---
+++ backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,26 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+const creditSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true }
+})
+
+const debtScchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true },
+    status: {
+        type: String, required: false, uppercase: true,
+        enum: ['PAGO', 'PENDENTE', 'AGENDADO']
+    }
+})
+
+const billingCycleSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1970, max: 2100, required: true },
+    credits: [creditSchema],
+    debits: [debtScchema]
+})
+
+module.exports = restful.model('BillingCycle', billingCycleSchema)
