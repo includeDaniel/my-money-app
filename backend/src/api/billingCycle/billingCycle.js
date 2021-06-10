2bd0bf461724eea348d2d9964cbb2e7edd187136
Daniel Nunes <danielzcomputacao@gmail.com>
2021-06-10 15:55:11 -0300

    summary route

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -20,7 +20,7 @@ const billingCycleSchema = new mongoose.Schema({
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
-    debits: [debtScchema]
+    debts: [debtScchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -28,4 +28,20 @@ BillingCycle.route('count', (req, res, next) => {
     })
 })
 
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    }, {
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }], (error, result) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json(result[0] || { credit: 0, debt: 0 })
+        }
+    })
+})
+
 module.exports = BillingCycle
