7a62106df4445dbd6a467d358022da876fcb9ef9
Daniel Nunes <danielzcomputacao@gmail.com>
2021-08-20 00:21:08 -0300

    sidebar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,12 @@
 import "../common/template/dependencies";
 import React from "react";
 import Header from "../common/template/header";
+import Sidebar from "../common/template/sideBar";
+import Menu from "../common/template/menu";
 
 export default (props) => (
   <div className="wrapper">
-    <Header>sd</Header>
+    <Header />
+    <Sidebar />
   </div>
 );
