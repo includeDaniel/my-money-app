e18881ef22ffe09012debfab9ef23053e324aed3
Daniel Nunes <danielzcomputacao@gmail.com>
2021-08-21 00:03:33 -0300

    footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy 2017
+      <a href="https://cod3r.com.br" target="_blank">
+        Cod3r
+      </a>
+    </strong>
+  </footer>
+);
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,11 +2,15 @@ import "../common/template/dependencies";
 import React from "react";
 import Header from "../common/template/header";
 import Sidebar from "../common/template/sideBar";
-import Menu from "../common/template/menu";
+import Footer from "../common/template/footer";
 
 export default (props) => (
   <div className="wrapper">
     <Header />
     <Sidebar />
+    <div className="content-wrapper">
+      <h1>Conteúdo</h1>
+    </div>
+    <Footer />
   </div>
 );
